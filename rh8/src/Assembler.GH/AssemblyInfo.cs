e4e3f121e27f2d86785b78979e63a4b482083016
Julia <weichingchen0703@gmail.com>
2025-03-05 14:52:14 -0500

    update code

---
+++ rh8/src/Assembler.GH/AssemblyInfo.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Reflection;
+using System.Runtime.Versioning;
+using System.Runtime.InteropServices;
+
+using Rhino.PlugIns;
+
+[assembly: PlugInDescription(DescriptionType.Organization, "")]
+[assembly: PlugInDescription(DescriptionType.Email, "wc683@cornell.edu")]
+[assembly: PlugInDescription(DescriptionType.Phone, "")]
+[assembly: PlugInDescription(DescriptionType.Address, "")]
+[assembly: PlugInDescription(DescriptionType.Country, "")]
+[assembly: PlugInDescription(DescriptionType.WebSite, "")]
+[assembly: PlugInDescription(DescriptionType.UpdateUrl, "")]
+[assembly: PlugInDescription(DescriptionType.Icon, "Assembler.GH.Resources.projectIcon.ico")]
+
+[assembly: AssemblyTitle("Assembler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Assembler")]
+[assembly: AssemblyCopyright("Copyright © 2024 Weiching Chen, Xiaoman Yang, Carrie Wang")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("02fe8045-bbb8-4621-93a2-dc314e0061f6")]
+[assembly: AssemblyVersion("1.0.2.9119")]
+[assembly: AssemblyFileVersion("1.0.2.9119")]
+[assembly: AssemblyInformationalVersion("1.0.2.9119")]
+
+[assembly: TargetFramework(".NETFramework,Version=v4.8", FrameworkDisplayName = ".NET Framework 4.8")]
---
+++ rh8/src/Assembler.GH/ProjectPlugin_Grasshopper.cs
@@ -0,0 +1,142 @@
+using System;
+using SD = System.Drawing;
+
+using Rhino;
+using Grasshopper.Kernel;
+
+namespace RhinoCodePlatform.Rhino3D.Projects.Plugin.GH
+{
+  public sealed class AssemblyInfo : GH_AssemblyInfo
+  {
+    public override Guid Id { get; } = new Guid("02fe8045-bbb8-4621-93a2-dc314e0061f6");
+
+    public override string AssemblyName { get; } = "Assembler.GH";
+    public override string AssemblyVersion { get; } = "1.0.2.9119";
+    public override string AssemblyDescription { get; } = "";
+    public override string AuthorName { get; } = "Weiching Chen";
+    public override string AuthorContact { get; } = "wc683@cornell.edu";
+    public override GH_LibraryLicense AssemblyLicense { get; } = GH_LibraryLicense.unset;
+    public override SD.Bitmap AssemblyIcon { get; } = ProjectComponentPlugin.PluginIcon;
+  }
+
+  public class ProjectComponentPlugin : GH_AssemblyPriority
+  {
+    public static SD.Bitmap PluginIcon { get; }
+    public static SD.Bitmap PluginCategoryIcon { get; }
+
+    static readonly Guid s_rhinocode = new Guid("c9cba87a-23ce-4f15-a918-97645c05cde7");
+    static readonly Type s_invokeContextType = default;
+    static readonly dynamic s_projectServer = default;
+    static readonly object s_project = default;
+
+    static readonly Guid s_projectId = new Guid("02fe8045-bbb8-4621-93a2-dc314e0061f6");
+    static readonly string s_projectData = "ewogICJpZCI6ICIwMmZlODA0NS1iYmI4LTQ2MjEtOTNhMi1kYzMxNGUwMDYxZjYiLAogICJpZGVudGl0eSI6IHsKICAgICJuYW1lIjogIkFzc2VtYmxlciIsCiAgICAidmVyc2lvbiI6ICIxLjAuMi1iZXRhIiwKICAgICJwdWJsaXNoZXIiOiB7CiAgICAgICJlbWFpbCI6ICJ3YzY4M0Bjb3JuZWxsLmVkdSIsCiAgICAgICJuYW1lIjogIldlaWNoaW5nIENoZW4iCiAgICB9LAogICAgImNvcHlyaWdodCI6ICJDb3B5cmlnaHQgXHUwMEE5IDIwMjQgV2VpY2hpbmcgQ2hlbiwgWGlhb21hbiBZYW5nLCBDYXJyaWUgV2FuZyIsCiAgICAibGljZW5zZSI6ICJNSVQiCiAgfSwKICAic2V0dGluZ3MiOiB7CiAgICAiYnVpbGRQYXRoIjogImZpbGU6Ly8vVXNlcnMvanVsaWEvRGVza3RvcC9Db3JuZWxsMjRGYWxsL2Rlc2lnbjYyOTdmaW5hbC9idWlsZC9yaDgiLAogICAgImJ1aWxkVGFyZ2V0IjogewogICAgICAiYXBwTmFtZSI6ICJSaGlubzNEIiwKICAgICAgImFwcFZlcnNpb24iOiB7CiAgICAgICAgIm1ham9yIjogOAogICAgICB9LAogICAgICAidGl0bGUiOiAiUmhpbm8zRCAoOC4qKSIsCiAgICAgICJzbHVnIjogInJoOCIKICAgIH0sCiAgICAicHVibGlzaFRhcmdldCI6IHsKICAgICAgInRpdGxlIjogIk1jTmVlbCBZYWsgU2VydmVyIgogICAgfQogIH0sCiAgImNvZGVzIjogW10KfQ==";
+    static readonly string _iconData = "[[ASSEMBLY-ICON]]";
+
+    static ProjectComponentPlugin()
+    {
+      s_projectServer = ProjectInterop.GetProjectServer();
+      if (s_projectServer is null)
+      {
+        RhinoApp.WriteLine($"Error loading Grasshopper plugin. Missing Rhino3D platform");
+        return;
+      }
+
+      // get project
+      dynamic dctx = ProjectInterop.CreateInvokeContext();
+      dctx.Inputs["projectAssembly"] = typeof(ProjectComponentPlugin).Assembly;
+      dctx.Inputs["projectId"] = s_projectId;
+      dctx.Inputs["projectData"] = s_projectData;
+
+      object project = default;
+      if (s_projectServer.TryInvoke("plugins/v1/deserialize", dctx)
+            && dctx.Outputs.TryGet("project", out project))
+      {
+        // server reports errors
+        s_project = project;
+      }
+
+      // get icons
+      if (!_iconData.Contains("ASSEMBLY-ICON"))
+      {
+        dynamic ictx = ProjectInterop.CreateInvokeContext();
+        ictx.Inputs["iconData"] = _iconData;
+        SD.Bitmap icon = default;
+        if (s_projectServer.TryInvoke("plugins/v1/icon/gh/assembly", ictx)
+              && ictx.Outputs.TryGet("icon", out icon))
+        {
+          // server reports errors
+          PluginIcon = icon;
+        }
+
+        if (s_projectServer.TryInvoke("plugins/v1/icon/gh/category", ictx)
+              && ictx.Outputs.TryGet("icon", out icon))
+        {
+          // server reports errors
+          PluginCategoryIcon = icon;
+        }
+      }
+    }
+
+    public override GH_LoadingInstruction PriorityLoad()
+    {
+      Grasshopper.Instances.ComponentServer.AddCategorySymbolName("Assembler", "Assembler"[0]);
+
+      if (PluginCategoryIcon != null)
+        Grasshopper.Instances.ComponentServer.AddCategoryIcon("Assembler", PluginCategoryIcon);
+
+      return GH_LoadingInstruction.Proceed;
+    }
+
+    public static bool TryCreateScript(GH_Component ghcomponent, string serialized, out object script)
+    {
+      script = default;
+
+      if (s_projectServer is null) return false;
+
+      dynamic dctx = ProjectInterop.CreateInvokeContext();
+      dctx.Inputs["component"] = ghcomponent;
+      dctx.Inputs["project"] = s_project;
+      dctx.Inputs["scriptData"] = serialized;
+
+      if (s_projectServer.TryInvoke("plugins/v1/gh/deserialize", dctx))
+      {
+        return dctx.Outputs.TryGet("script", out script);
+      }
+
+      return false;
+    }
+
+    public static bool TryCreateScriptIcon(object script, out SD.Bitmap icon)
+    {
+      icon = default;
+
+      if (s_projectServer is null) return false;
+
+      dynamic ictx = ProjectInterop.CreateInvokeContext();
+      ictx.Inputs["script"] = script;
+
+      if (s_projectServer.TryInvoke("plugins/v1/icon/gh/script", ictx))
+      {
+        // server reports errors
+        return ictx.Outputs.TryGet("icon", out icon);
+      }
+
+      return false;
+    }
+
+    public static void DisposeScript(GH_Component ghcomponent, object script)
+    {
+      if (script is null)
+        return;
+
+      dynamic dctx = ProjectInterop.CreateInvokeContext();
+      dctx.Inputs["component"] = ghcomponent;
+      dctx.Inputs["project"] = s_project;
+      dctx.Inputs["script"] = script;
+
+      if (!s_projectServer.TryInvoke("plugins/v1/gh/dispose", dctx))
+        throw new Exception("Error disposing Grasshopper script component");
+    }
+  }
+}
